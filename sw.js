0d28c24c1ac50d8c94884a539d3d8eb9eae6db37
Pedro Lima <pmc.lima@hotmail.com>
2025-05-20 23:12:52 +0100

    test

--- sw.js
+++ sw.js
@@ -1,9 +1,16 @@
-const CACHE_NAME = 'image-gallery-cache-v1';
+const CACHE_NAME = 'dnd-classes-cache-v1.0.7';
+
+// Ordered list of resources to cache with prioritization
 const urlsToCache = [
   '/',
   '/index.html',
   '/style.css',
+  '/script.js',
+  '/image-offsets.json',
+  
+  // Main classes in order of appearance
   '/classes/Barbarian.png',
+  '/classes/Bard.png',
   '/classes/Cleric.png',
   '/classes/Druid.png',
   '/classes/Fighter.png',
@@ -14,117 +21,128 @@ const urlsToCache = [
   '/classes/Sorcerer.png',
   '/classes/Warlock.png',
   '/classes/Wizard.png',
-  '/subclasses/Aberrant Sorcerer.png',
-  '/subclasses/Abjurer Wizard.png',
-  '/subclasses/Ancients Paladin.png',
-  '/subclasses/Arcane Trickster Rogue.png',
-  '/subclasses/Archfey Warlock.png',
-  '/subclasses/Assassin Rogue.png',
-  '/subclasses/Battle Master Fighter.png',
-  '/subclasses/Beast Master Ranger.png',
+  
+  // Barbarian subclasses (first class)
   '/subclasses/Berserker Barbarian.png',
-  '/subclasses/Celestial Warlock.png',
-  '/subclasses/Champion Fighter.png',
-  '/subclasses/Clockwork Sorcerer.png',
+  '/subclasses/Wild Heart Barbarian.png',
+  '/subclasses/World Tree Barbarian.png',
+  '/subclasses/Zealot Barbarian.png',
+  
+  // Bard subclasses (second class)
   '/subclasses/Dance Bard.png',
-  '/subclasses/Devotion Paladin.png',
-  '/subclasses/Diviner Wizard.png',
-  '/subclasses/Draconic Sorcerer.png',
-  '/subclasses/Eldritch Knight Fighter.png',
-  '/subclasses/Elements Monk.png',
-  '/subclasses/Evoker Wizard.png',
-  '/subclasses/Fey Wanderer Ranger.png',
-  '/subclasses/Fiend Warlock.png',
   '/subclasses/Glamour Bard.png',
-  '/subclasses/Gloom Stalker Ranger.png',
-  '/subclasses/Glory Paladin.png',
-  '/subclasses/Great Old One Warlock.png',
-  '/subclasses/Hunter Ranger.png',
-  '/subclasses/Illusionist Wizard.png',
-  '/subclasses/Land Druid.png',
+  '/subclasses/Lore Bard.png',
+  '/subclasses/Valor Bard.png',
+  
+  // Remaining subclasses in order
   '/subclasses/Life Cleric.png',
   '/subclasses/Light Cleric.png',
-  '/subclasses/Lore Bard.png',
-  '/subclasses/Mercy Monk.png',
-  '/subclasses/Moon Druid.png',
-  '/subclasses/Open Hand Monk.png',
-  '/subclasses/Psi Warrior Fighter.png',
-  '/subclasses/Sea Druid.png',
-  '/subclasses/Shadow Monk.png',
-  '/subclasses/Soulknife Rogue.png',
-  '/subclasses/Stars Druid.png',
-  '/subclasses/Thief Rogue.png',
   '/subclasses/Trickery Cleric.png',
-  '/subclasses/Valor Bard.png',
-  '/subclasses/Vengeance Paladin.png',
   '/subclasses/War Cleric.png',
-  '/subclasses/Wild Heart Barbarian.png',
+  
+  '/subclasses/Land Druid.png',
+  '/subclasses/Moon Druid.png',
+  '/subclasses/Sea Druid.png',
+  '/subclasses/Stars Druid.png',
+  
+  '/subclasses/Battle Master Fighter.png',
+  '/subclasses/Champion Fighter.png',
+  '/subclasses/Eldritch Knight Fighter.png',
+  '/subclasses/Psi Warrior Fighter.png',
+  
+  '/subclasses/Mercy Monk.png',
+  '/subclasses/Shadow Monk.png',
+  '/subclasses/Elements Monk.png',
+  '/subclasses/Open Hand Monk.png',
+  
+  '/subclasses/Devotion Paladin.png',
+  '/subclasses/Glory Paladin.png',
+  '/subclasses/Ancients Paladin.png',
+  '/subclasses/Vengeance Paladin.png',
+  
+  '/subclasses/Beast Master Ranger.png',
+  '/subclasses/Fey Wanderer Ranger.png',
+  '/subclasses/Gloom Stalker Ranger.png',
+  '/subclasses/Hunter Ranger.png',
+  
+  '/subclasses/Arcane Trickster Rogue.png',
+  '/subclasses/Assassin Rogue.png',
+  '/subclasses/Soulknife Rogue.png',
+  '/subclasses/Thief Rogue.png',
+  
+  '/subclasses/Aberrant Sorcerer.png',
+  '/subclasses/Clockwork Sorcerer.png',
+  '/subclasses/Draconic Sorcerer.png',
   '/subclasses/Wild Magic Sorcerer.png',
-  '/subclasses/World Tree Barbarian.png',
-  '/subclasses/Zealot Barbarian.png'
+  
+  '/subclasses/Archfey Warlock.png',
+  '/subclasses/Celestial Warlock.png',
+  '/subclasses/Fiend Warlock.png',
+  '/subclasses/Great Old One Warlock.png',
+  
+  '/subclasses/Abjurer Wizard.png',
+  '/subclasses/Diviner Wizard.png',
+  '/subclasses/Evoker Wizard.png',
+  '/subclasses/Illusionist Wizard.png'
 ];
 
+// Install event: cache resources in order of priority
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+      .then(() => {
+        return self.skipWaiting();
+      })
   );
 });
 
-self.addEventListener('fetch', event => {
-  // Cache images with a cache-first strategy
-  if (event.request.url.match(/\.(jpg|jpeg|png|gif|webp|svg)$/i)) {
-    event.respondWith(
-      caches.match(event.request)
-        .then(response => {
-          // Return cached response if found
-          if (response) {
-            return response;
-          }
-          
-          // Otherwise fetch from network and cache
-          return fetch(event.request).then(
-            response => {
-              // Check if we received a valid response
-              if (!response || response.status !== 200 || response.type !== 'basic') {
-                return response;
-              }
-              
-              // Clone the response as it can only be consumed once
-              const responseToCache = response.clone();
-              
-              caches.open(CACHE_NAME)
-                .then(cache => {
-                  cache.put(event.request, responseToCache);
-                });
-                
-              return response;
-            }
-          );
-        })
-    );
-  } else {
-    // Use network-first for other resources
-    event.respondWith(
-      fetch(event.request).catch(() => {
-        return caches.match(event.request);
-      })
-    );
-  }
-});
-
-// Clean up old caches
+// Activate event: clean up old caches
 self.addEventListener('activate', event => {
-  const cacheWhitelist = [CACHE_NAME];
   event.waitUntil(
     caches.keys().then(cacheNames => {
       return Promise.all(
         cacheNames.map(cacheName => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
+          if (cacheName !== CACHE_NAME) {
             return caches.delete(cacheName);
           }
         })
       );
+    }).then(() => {
+      return self.clients.claim();
     })
+  );
+});
+
+// Fetch event: serve from cache first, then network
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request).then(
+          response => {
+            // Don't cache responses that aren't successful or aren't GET requests
+            if (!response || response.status !== 200 || response.type !== 'basic' || 
+                event.request.method !== 'GET') {
+              return response;
+            }
+
+            // Clone response since it's a stream and can only be consumed once
+            const responseToCache = response.clone();
+            
+            caches.open(CACHE_NAME)
+              .then(cache => {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
   );
 });
